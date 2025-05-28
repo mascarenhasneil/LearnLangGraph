a9586d4b5bdbe4870500d41ebc151b651cd7d6db
Neil Mascarenhas <neilpm1995@gmail.com>
2025-05-28 18:49:12 -0400

    feat(simple_bot): enhance agent state processing with improved message handling

--- simple_bot.py
+++ simple_bot.py
@@ -4,7 +4,7 @@ Objectives:
  2. Initialize a GPT-4o model using LangChain's ChatOpenAI
  3. Sending and handling different types of messages
  4. Building and compiling the graph of the Agent
- 
+
  Main Goal: How to integrate LLMs in our Graphs
 
 """
@@ -14,34 +14,43 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, START, END
 
-from dotenv import load_dotenv # we use it for loading environment variables secrets and stuff.
+from dotenv import (
+    load_dotenv,
+)  # we use it for loading environment variables secrets and stuff.
 
 
 load_dotenv()  # Load environment variables from a .env file
 
+
 class AgentState(TypedDict):
-    """ Initial state of the agent with a list of human messages. """
+    """Initial state of the agent with a list of human messages."""
+
     messages: List[HumanMessage]
 
-llm = ChatOpenAI(model="gpt-4.1-nano", temperature=0.0,)
+
+llm = ChatOpenAI(
+    model="gpt-4.1-nano",
+    temperature=0.0,
+)
 
 
 def process(state: AgentState) -> AgentState:
     """Process the agent's state and generate a response"""
-    
+
     response = llm.invoke(state["messages"])
     print(f"\nAI: {response.content}\n")
-    
+
     return state
 
+
 graph = StateGraph(AgentState)
-graph.add_node(node = "process", action = process)
-graph.add_edge(start_key = START, end_key = "process")
-graph.add_edge(start_key = "process", end_key = END)
+graph.add_node(node="process", action=process)
+graph.add_edge(start_key=START, end_key="process")
+graph.add_edge(start_key="process", end_key=END)
 
 agent = graph.compile()
 
-user_input  = input("You: ")
+user_input = input("You: ")
 
-# The code above initializes a simple agent that processes human messages using a GPT-4.1 nano model.
-agent.invoke( input = {"messages": [HumanMessage(content = user_input)]})
+# code above initializes a simple agent that processes human messages using a GPT-4.1 nano model.
+agent.invoke(input={"messages": [HumanMessage(content=user_input)]})
